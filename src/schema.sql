73cead3c8298c8843484e552968b48fd1cc3ccea
waj-faqirzada <wajeedullah.faqirzad@ada.ac.uk>
2026-02-05 12:07:36 +0000

    cretaed a table

--- src/schema.sql
+++ src/schema.sql
@@ -2,9 +2,13 @@
 .mode column
 
 CREATE TABLE locations (
-    location_id INTEGER NOT NULL
-    name TEXT NOT NULL
-    adress TEXT NOT NULL
-    phone_number TEXT NOT NULL
-    email TEXT NOT NULL UNIQUE
-    
+    location_id INTEGER NOT NULL,
+    name TEXT NOT NULL,
+    adress TEXT NOT NULL,
+    phone_number TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    opening_hours 
+
+INSERT INTO location_id,name,address,phone_number,email,opening_hours
+1,"Downtown Fitness","123 Main St, London","020 555 1234",downtown@fittrackpro.com,06:00-22:00
+2,"Suburban Wellness","45 Oak Ln, Manchester","0161 555 5678",suburban@fittrackpro.com,05:00-23:00
