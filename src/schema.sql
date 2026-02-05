1f5bc0014d1411dc4a8510f59f7bb2d8da8c7749
waj-faqirzada <wajeedullah.faqirzad@ada.ac.uk>
2026-02-05 14:34:30 +0000

    fixed table

--- src/schema.sql
+++ src/schema.sql
@@ -1,15 +1,17 @@
 .open fittrackpro.db
 .mode column
 
+DROP TABLE IF EXISTS locations;
 CREATE TABLE locations (
-    location_id INTEGER PRIMARY NOT NULL,
+    location_id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(50) NOT NULL,
-    address VARCHAR(50) NOT NULL,
+    address VARCHAR(255) NOT NULL,
     phone_number VARCHAR(20) NOT NULL,
     email VARCHAR(150) NOT NULL UNIQUE,
     opening_hours VARCHAR(20) NOT NULL
 );
 
-INSERT INTO location_id,name,address,phone_number,email,opening_hours
-1,"Downtown Fitness","123 Main St, London","020 555 1234",downtown@fittrackpro.com,06:00-22:00
-2,"Suburban Wellness","45 Oak Ln, Manchester","0161 555 5678",suburban@fittrackpro.com,05:00-23:00
+INSERT INTO locations (name,address,phone_number,email,opening_hours)
+VALUES
+('Downtown Fitness','123 Main St, London','020 555 1234','downtown@fittrackpro.com','06:00-22:00'),
+('Suburban Wellness','45 Oak Ln, Manchester','0161 555 5678','suburban@fittrackpro.com','05:00-23:00');
