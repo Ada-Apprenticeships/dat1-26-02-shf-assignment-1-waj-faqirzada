c95bfee8d3a62f3295a36d6158e81a689656317b
waj-faqirzada <wajeedullah.faqirzad@ada.ac.uk>
2026-02-06 11:28:36 +0000

    UPDATED

--- src/schema.sql
+++ src/schema.sql
@@ -7,23 +7,23 @@ CREATE TABLE locations (
     name VARCHAR(50) NOT NULL,
     address VARCHAR(255) NOT NULL,
     phone_number VARCHAR(20) NOT NULL,
-    email VARCHAR(150) NOT NULL UNIQUE,
-    opening_hours VARCHAR(20) NOT NULL
+    email VARCHAR(150) NOT NULL UNIQUE CHECK (email LIKE '%@%.%'),
+    opening_hours VARCHAR(20) NOT NULL CHECK (opening_hours LIKE '__:__-__:__')
 );
 
--- INSERT INTO locations (location_id,name,address,phone_number,email,opening_hours)
--- VALUES
--- (1, 'Downtown Fitness','123 Main St, London','020 555 1234','downtown@fittrackpro.com','06:00-22:00'),
--- (2, 'Suburban Wellness','45 Oak Ln, Manchester','0161 555 5678','suburban@fittrackpro.com','05:00-23:00');
+INSERT INTO locations (location_id,name,address,phone_number,email,opening_hours)
+VALUES
+(1, 'Downtown Fitness','123 Main St, London','020 555 1234','downtown@fittrackpro.com','06:00-22:00'),
+(2, 'Suburban Wellness','45 Oak Ln, Manchester','0161 555 5678','suburban@fittrackpro.com','05:00-23:00');
 
 
-DROP TABLE IF EXISTS members;
-CREATE TABLE members (
-    member_id
-    first_name
-    last_name
-    email VARCHAR(150) NOT NULL UNIQUE,
-    phone_number VARCHAR(20) NOT NULL,
-    date_of_birth VARCHAR
-    join_date varchar
+-- DROP TABLE IF EXISTS members;
+-- CREATE TABLE members (
+--     member_id
+--     first_name
+--     last_name
+--     email VARCHAR(150) NOT NULL UNIQUE,
+--     phone_number VARCHAR(20) NOT NULL,
+--     date_of_birth VARCHAR
+--     join_date varchar
     
