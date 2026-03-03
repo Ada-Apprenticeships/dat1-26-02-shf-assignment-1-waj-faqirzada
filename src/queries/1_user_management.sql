ebf66ec2406b995bdec8181be7e3b544c088f90e
waj-faqirzada <wajeedullah.faqirzad@ada.ac.uk>
2026-03-03 21:09:54 +0000

    done equipment managment

--- src/queries/1_user_management.sql
+++ src/queries/1_user_management.sql
@@ -2,7 +2,7 @@
 .mode column
 
 -- 1.1-Retrieve all members
-SELECT member_id, first_name,last_name,email,join_date
+SELECT member_id, first_name, last_name, email, join_date
 FROM members;
 
 -- 1.2-Update a member's contact information
@@ -15,7 +15,7 @@ SELECT COUNT(*) AS total_members
 FROM members;
 
 -- 1.4-Find member with the most class registrations
-SELECT m.member_id, m.first_name,m.last_name, COUNT (ca.class_attendance_id) AS registration_count --m.member_id is saying look for member_id in the members table 
+SELECT m.member_id, m.first_name, m.last_name, COUNT (ca.class_attendance_id) AS registration_count --m.member_id is saying look for member_id in the members table 
 FROM members m
 JOIN class_attendance ca ON m.member_id = ca.member_id
 GROUP BY m.member_id
@@ -24,7 +24,7 @@ LIMIT 1; -- will stop when the data your looking for is found rather then contin
 
 
 -- 1.5-Find member with the least class registrations
-SELECT m.member_id, m.first_name,m.last_name, COUNT (ca.class_attendance_id) AS registration_count 
+SELECT m.member_id, m.first_name, m.last_name, COUNT (ca.class_attendance_id) AS registration_count 
 FROM members m
 JOIN class_attendance ca ON m.member_id = ca.member_id
 GROUP BY m.member_id
--- src/queries/3_equipment_management.sql
+++ src/queries/3_equipment_management.sql
@@ -1,11 +1,19 @@
 .open fittrackpro.db
 .mode column
 
--- 3.1 
+-- 3.1- Find equipment due for maintenance in the next 30 days 
+SELECT equipment_id, name, next_maintenance_date
+FROM equipment
+WHERE next_maintenance_date BETWEEN '2025-01-01' AND DATE('2025-01-01', '+30 days');
 
+-- 3.2 -Count equipment types in stock
+SELECT equipment_type, COUNT(*) AS count
+FROM equipment
+GROUP BY equipment_type;
 
--- 3.2 
-
-
--- 3.3 
-
+-- 3.3 -Calculate average age of equipment by type (in days)
+ SELECT 
+    equipment_type,
+    AVG(JULIANDAY('now') - JULIANDAY(purchase_date)) AS avg_age_days
+FROM equipment
+GROUP BY equipment_type;
