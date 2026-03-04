2f3b326bae57ba0612a25e252cfc12f6a5b380f5
waj-faqirzada <wajeedullah.faqirzad@ada.ac.uk>
2026-03-04 11:21:25 +0000

    attempted 8

--- src/queries/8_personal_training.sql
+++ src/queries/8_personal_training.sql
@@ -1,5 +1,13 @@
 .open fittrackpro.db
 .mode column
 
--- 8.1 
-
+-- 8.1 -List all personal training sessions for specific trainer "Ivy Irwin"
+SELECT 
+    pts.session_id s.first_name || ' ' || s.last_name AS member_name,
+    pts.session_date,
+    pts.start_time,
+    pts.end_time,
+FROM personal_training_sessions pts
+JOINS staff s ON pts>staff_id = s.staff_id
+JOIN members m pts.member_id = m.member_id
+WHERE s.first_name = 'Ivy' AND s.last_name = 'Irwin';
