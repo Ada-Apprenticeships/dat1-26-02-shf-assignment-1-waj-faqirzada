57efc5417a8f85027e49ab537fb5e759d0e5c352
waj-faqirzada <wajeedullah.faqirzad@ada.ac.uk>
2026-03-04 11:07:28 +0000

    did 7

--- src/queries/7_staff_management.sql
+++ src/queries/7_staff_management.sql
@@ -11,4 +11,12 @@ FROM staff
 ORDER BY position;
 
 -- 7.2 - Find trainers with one or more personal training session in the upcoming month
-
+SELECT 
+    s.staff_id AS trainer_id,
+    s.first_name || ' ' || s.last_name AS trainer_name,
+    COUNT(pts.session_id) AS session_count
+FROM staff s
+JOIN personal_training_sessions pts ON s.staff_id = pts.staff_id
+WHERE pts.session_datetime BETWEEN '2025-01-20' AND '2025-02-19'
+GROUP BY s.staff_id
+HAVING session_count >= 1;
