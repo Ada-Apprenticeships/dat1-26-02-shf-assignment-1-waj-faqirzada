01bdfdecd04e71b1c054f368cf4f404589a0257f
waj-faqirzada <wajeedullah.faqirzad@ada.ac.uk>
2026-03-04 11:16:15 +0000

    fixed error

--- src/queries/7_staff_management.sql
+++ src/queries/7_staff_management.sql
@@ -17,6 +17,6 @@ SELECT
     COUNT(pts.session_id) AS session_count
 FROM staff s
 JOIN personal_training_sessions pts ON s.staff_id = pts.staff_id
-WHERE pts.session_datetime BETWEEN '2025-01-20' AND '2025-02-19'
+WHERE pts.session_date >= date(2025-01-20) AND date (025-02-19)
 GROUP BY s.staff_id
 HAVING session_count >= 1;
